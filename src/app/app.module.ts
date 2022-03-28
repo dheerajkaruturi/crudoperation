9325e73916a600cf405dbbfd762e77bfb99fbe4d
dheeraj karuturi <dheeraj.karuturi@audreeinfotech.com>
2022-03-28 09:58:32 +0530

    environment created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateContactComponent } from './create-contact/create-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { DeleteContactComponent } from './delete-contact/delete-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateContactComponent,
+    EditContactComponent,
+    DeleteContactComponent
   ],
   imports: [
     BrowserModule,
